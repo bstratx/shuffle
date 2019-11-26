19f3c85955b52817f6d1cf45cdb8bc8ae87abbb8
Bill Stratford <bills@01MJG8WL.hsd1.wa.comcast.net>
2019-11-26 00:31:50 -0800

    minor tidying

--- src/DeckShuffler.java
+++ src/DeckShuffler.java
@@ -2,10 +2,8 @@ import java.util.Random;
 
 public class DeckShuffler {
 
-    private static final int CARDS_IN_SUIT = 13;
-    private static final int SUITS_IN_DECK = 4;
-    private static final int CARDS_IN_DECK = CARDS_IN_SUIT * SUITS_IN_DECK;
-    private static final String CARD_NAMES[] = {
+
+    private static final String[] CARD_NAMES = {
         "2",
         "3",
         "4",
@@ -21,32 +19,18 @@ public class DeckShuffler {
         "A",
     };
 
-    private static final String SUIT_NAMES[] = {
-            "S",
-            "H",
-            "D",
-            "C"
+    private static final String[] SUIT_NAMES = {
+        "S",
+        "H",
+        "D",
+        "C"
     };
 
+    private static final int CARDS_IN_SUIT = CARD_NAMES.length;
+    private static final int SUITS_IN_DECK = SUIT_NAMES.length;
+    private static final int CARDS_IN_DECK = CARDS_IN_SUIT * SUITS_IN_DECK;
+
     private static final Random rand = new Random();
-
-    private static void printCard(int cardNumber) {
-        final int suitIndex = cardNumber/CARDS_IN_SUIT;
-        final int cardIndex = cardNumber - (suitIndex * CARDS_IN_SUIT);
-        System.out.println("(" + cardNumber + "):" + CARD_NAMES[cardIndex] + " of " + SUIT_NAMES[suitIndex]);
-    }
-
-    private static int[] shuffleDeck(int[] inputDeck) {
-        int[] shuffledDeck = inputDeck.clone();
-        for (int currentIndex = 0; currentIndex < CARDS_IN_DECK; currentIndex++) {
-            int curCard = shuffledDeck[currentIndex];
-            int swapIndex = rand.nextInt(CARDS_IN_DECK);
-            int swapCard = shuffledDeck[swapIndex];
-            shuffledDeck[currentIndex] = swapCard;
-            shuffledDeck[swapIndex] = curCard;
-        }
-        return shuffledDeck;
-    }
 
     private static int[] initializeDeck() {
         int[] deck = new int[CARDS_IN_DECK];
@@ -54,6 +38,23 @@ public class DeckShuffler {
             deck[i] = i;
         }
         return deck;
+    }
+
+    private static void printCard(int cardNumber) {
+        final int suitIndex = cardNumber/CARDS_IN_SUIT;
+        final int cardIndex = cardNumber - (suitIndex * CARDS_IN_SUIT);
+        System.out.println(CARD_NAMES[cardIndex] + "." + SUIT_NAMES[suitIndex]);
+    }
+
+    private static int[] shuffleDeck(int[] inputDeck) {
+        int[] shuffledDeck = inputDeck.clone();
+        for (int currentIndex = 0; currentIndex < CARDS_IN_DECK; currentIndex++) {
+            int currentCard = shuffledDeck[currentIndex];
+            int swapIndex = rand.nextInt(CARDS_IN_DECK);
+            shuffledDeck[currentIndex] = shuffledDeck[swapIndex];
+            shuffledDeck[swapIndex] = currentCard;
+        }
+        return shuffledDeck;
     }
 
     public static void main(String[] args) {
